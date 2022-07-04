73ab92f3592e5de39cce54a832870b263ab83ccc
Regijus <regijusb@gmail.com>
2022-07-04 21:59:54 +0300

    Publish version 0.0.2 of the design system

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="carousel-wrapper">
     <RbCarousel
       :size="$options.CAROUSEL_SIZE"
       :slide-interval-time="$options.CAROUSEL_SLIDE_INTERVAL_TIME"
-      class="carousel-v1"
+      class="carousel"
     >
       <RbCarouselItem
         v-for="(carouselItem, index) in carouselItems"
         :key="`carousel-item-${index}`"
-        :image-path="carouselItem.imagePath"
+        :image-url="carouselItem.imageUrl"
         :image-alt="carouselItem.imageAlt"
       />
     </RbCarousel>
@@ -16,6 +16,7 @@
       :items="carouselItems"
       :size="$options.CAROUSEL_SIZE"
       :slide-interval-time="$options.CAROUSEL_SLIDE_INTERVAL_TIME"
+      class="carousel"
     />
   </div>
 </template>
@@ -39,19 +40,19 @@ export default {
     carouselItems() {
       return [
         {
-          imagePath: 'https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+          imageUrl: 'https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
           imageAlt: 'Mountainy island in the middle of a sea',
         },
         {
-          imagePath: 'https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+          imageUrl: 'https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
           imageAlt: 'Mountains with a parking below them',
         },
         {
-          imagePath: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+          imageUrl: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
           imageAlt: 'Snowy mountains with a comet falling down in the night sky',
         },
         {
-          imagePath: 'https://images.unsplash.com/photo-1576225410873-a28b2a79fd93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+          imageUrl: 'https://images.unsplash.com/photo-1576225410873-a28b2a79fd93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
           imageAlt: 'Bridge with a big fog above it',
         },
       ];
@@ -70,7 +71,14 @@ export default {
   margin-top: 60px;
 }
 
-.carousel-v1 {
+.carousel-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.carousel {
   margin-bottom: 64px;
 }
 </style>
